43c6449e4d353f743b9aced366d116a6e536a855
Waqar Khan <sms.raees@gmail.com>
2022-09-07 20:55:57 +0500

    added templates and other static files

--- stempro/school_front/views.py
+++ stempro/school_front/views.py
@@ -5,3 +5,16 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "school_front/index.html"
+
+def all_templates(request, template_name):
+    template_name = f"school_front/{template_name}"
+    return render(request, template_name)
+
+
+def all_templates_with_dir(request, template_dir, template_name):
+    template_name = f"school_front/{template_dir}/{template_name}"
+    return render(request, template_name)
+
+def all_templates_with_dir_dir(request, template_dir1, template_dir2, template_name):
+    template_name = f"school_front/{template_dir1}/{template_dir2}/{template_name}"
+    return render(request, template_name)
