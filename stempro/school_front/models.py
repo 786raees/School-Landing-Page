18352ea28faa804ee0f531c3901da3ae514d5a93
Waqar Khan <sms.raees@gmail.com>
2022-09-18 09:08:00 +0500

    added models for index

--- stempro/school_front/models.py
+++ stempro/school_front/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class IndexPage(models.Model):
+    # section 1
+    main_heading = models.CharField(_("Main Heading"), max_length=500, default="Building Powerful Minds Through Mathematics")
+    main_description = models.TextField(_("Hero Description"), default="""Our award winning K-12 after-school math program has empowered students to achieve excellence for over twenty years.""")
+    main_side_image = models.ImageField(_("Side Image"), help_text="Upload PNG", upload_to='uploads', default="https://russianschool-tinypng.s3.us-east-1.amazonaws.com/uploads/home2-min1576261723-2x1583938060-2x.1584953668_1x.png")
+    # section 2
+    rsm_online = models.CharField(_("Title"), max_length=50, default="RSM Online")
+    rsm_phone = models.CharField(_("Phone No"), max_length=50, default="(617) 362 3500")
+    rsm_link = models.URLField(_("Link"), max_length=50, default="online.elementary@russianschool.com")
+    # section 3
+    our_program_heading = models.CharField(_("Header"), max_length=50, default="Our Programs")
+    our_program_description = models.TextField(_("Description"), default="""With multiple levels for every grade as well as a selective competitions program, we are able to best serve each child’s development based on his or her knowledge and ability.""")
+    # section 4
+    our_result_heading = models.CharField(_("Header"), max_length=50, default="Our Results")
+    our_result_description = models.TextField(_("Description"), default="""Our students post remarkable scores on math competitions, see higher grades in school, and build a lasting confidence in their math and learning abilities overall.""")
+
+
+class Result(models.Model):
+    page = models.ForeignKey(IndexPage, verbose_name=_("Page"), on_delete=models.CASCADE)
+    result_heading = models.TextField(_("Heading"), default="A+")
+    description = models.CharField(_("Description"), max_length=50, default="Our students experience soaring confidence and grades")
+
+    def __str__(self):
+        return f"{self.result_heading}"
+
+class Review(models.Model):
+    page = models.ForeignKey(IndexPage, verbose_name=_("Page"), on_delete=models.CASCADE)
+    review_content = models.TextField(_("Review"), default="Students are required to think their way through logic problems that can be resolved only with creative use of the math they’ve learned.")
+    reviewer = models.CharField(_("Reviewer"), max_length=50, default="The Boston Globe")
+
+    def __str__(self):
+        return f"{self.reviewer}"
+    
+
+    
